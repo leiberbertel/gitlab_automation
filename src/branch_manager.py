5f438833173212e4038379d2d99ca24043b526bf
leiber bertel <leiberbertel777@gmail.com>
2025-02-03 22:41:07 -0500

    docs: add documentation of methods and classes

--- src/branch_manager.py
+++ src/branch_manager.py
@@ -1,8 +1,36 @@
 class BranchManager:
+    """Handles branch management operations in GitLab.
+
+    This class provides methods to create and manage branches
+    using a GitLab client.
+
+    Attributes:
+        client: An instance of the GitLab client used to interact
+                with the GitLab API.
+    """
     def __init__(self, gitlab_client):
+        """Initializes the BranchManager with a GitLab client.
+
+        Args:
+            gitlab_client: A GitLab client instance to communicate with
+                           the GitLab API.
+        """
         self.client = gitlab_client
 
-
     def create_branch(self, project_id, branch_name, ref):
+        """Creates a new branch in the specified GitLab project.
+        
+        Args:
+            project_id (int): The ID of the GitLab project.
+            branch_name (str): The name of the new branch.
+            ref (str): The reference (commit SHA, branch, or tag)
+                       from which the new branch will be created.
+
+        Returns:
+            None
+
+        Raises:
+            Exception: If branch creation fails.
+        """
         project = self.client.get_project(project_id)
         project.branches.create({'branch': branch_name, 'ref': ref})
--- src/gitlab_client.py
+++ src/gitlab_client.py
@@ -1,9 +1,35 @@
 import gitlab
 
 class GitLabClient:
+    """A client to interact with the GitLab API.
+    
+    This class provides methods to connect to a GitLab instance
+    and retrieve project information.
+    
+    Attributes:
+        gl (gitlab.Gitlab): An authenticated GitLab client instance.
+    """
+
     def __init__(self, url, private_token):
+        """Initializes the GitLab client.
+        
+        Args:
+            url (str): The GitLab instance URL.
+            private_token (str): The private token for authentication.
+        """
         self.gl = gitlab.Gitlab(url, private_token=private_token)
 
-
     def get_project(self, project_id):
+        """Retrieves a project from GitLab by its ID.
+        
+        Args:
+            project_id (int): The ID of the GitLab project.
+        
+        Returns:
+            gitlab.v4.objects.Project: The requested project instance.
+        
+        Raises:
+            gitlab.exceptions.GitlabGetError: If the project is not found or
+                                              the request fails.
+        """
         return self.gl.projects.get(project_id)
